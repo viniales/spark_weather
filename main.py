0401ae81b1fe499b1a0dd25a21399279b5d469bc
Viniales12 <viniales12@gmail.com>
2024-04-26 17:02:47 +0200

    base code

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, lit
+from pyspark.sql.types import DoubleType
+
+def get_weather(city):
+    api_key = "aabcce4f2010458c501e4f8230f90fae"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    if "main" in data and "temp" in data["main"]:
+        return data["main"]["temp"], data["name"], round(float(data["main"]["temp_max"]) - 273.15, 1)
+    else:
+        return None, None
+
+spark = SparkSession.builder \
+    .appName("TemperatureStreaming") \
+    .getOrCreate()
+
+# Define the schema for temperature data
+schema = DoubleType()
+
+
+weather_stream = spark \
+    .readStream \
+    .format("rate") \
+    .option("rowsPerSecond", 1) \
+    .load() \
+    .withColumn("temperature", lit(get_weather("Paris")[0]).cast(DoubleType())) \
+    .withColumn("city", lit(get_weather("Paris")[1])) \
+    .withColumn("temp_max", lit(get_weather("Paris")[2]))
+
+
+query = weather_stream \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
+
+query.awaitTermination()
